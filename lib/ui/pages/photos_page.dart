5027ebdd83ea9001b5f48e8ddd9c87ec9b7cb862
塩見陵介 <ryosukesukairain1@gmail.com>
2025-02-04 17:13:57 +0900

    手書き画面追加

--- lib/ui/pages/photos_page.dart
+++ lib/ui/pages/photos_page.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:signature/signature.dart';
 
-class PhotosPage extends StatelessWidget {
+class PhotosPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final SignatureController _controller = useMemoized(
+      () => SignatureController(
+        penStrokeWidth: 5,
+        penColor: Colors.red,
+        exportBackgroundColor: Colors.blue,
+      ),
+    );
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Photos'),
       ),
-      body: Center(
-        child: Text('Photos Page'),
+      body: Column(
+        children: [
+          Center(
+            child: Text('手書き画面'),
+          ),
+          Signature(
+            controller: _controller,
+            width: 300,
+            height: 300,
+            backgroundColor: Colors.lightBlueAccent,
+          ),
+        ],
       ),
     );
   }
