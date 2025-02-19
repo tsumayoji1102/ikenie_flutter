fd7cee4c8d43b67b1dff2c10a12e1f57a5b2c573
塩見陵介 <ryosukesukairain1@gmail.com>
2025-02-20 00:19:18 +0900

    mixinの練習

--- lib/model/sample.dart
+++ lib/model/sample.dart
@@ -3,8 +3,24 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'sample.freezed.dart';
 part 'sample.g.dart';
 
+mixin FreezedChangeRequestMixin {
+  Map<String, dynamic> requestJson({
+    required Map<String, dynamic> original,
+    required Map<String, dynamic> updated,
+  }) {
+    final result = <String, dynamic>{};
+    for (final key in updated.keys) {
+      if (original[key] != updated[key]) {
+        result[key] = updated[key];
+      }
+    }
+    return result;
+  }
+}
+
 @freezed
-class Sample with _$Sample {
+class Sample with _$Sample, FreezedChangeRequestMixin {
+  const Sample._();
   const factory Sample({
     required String id,
     required String name,
@@ -12,4 +28,9 @@ class Sample with _$Sample {
   }) = _Sample;
 
   factory Sample.fromJson(Map<String, dynamic> json) => _$SampleFromJson(json);
+
+  Map<String, dynamic> requestJsonA(Sample original) => requestJson(
+        original: original.toJson(),
+        updated: toJson(),
+      );
 }
